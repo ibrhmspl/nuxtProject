519be221a06d7124fcb8a6b48e913341f007c939
ibrhmspl <sipal23570@gmail.com>
2025-01-02 01:39:23 +0300

    github deneme

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@ const sepetStore = useCartStore();
 const showSepetDrawer = ref(false);
 const showModal = ref(false);
 const products = ref([]);
+const  deneme = "github"
 
 
 const newProduct = ref({
